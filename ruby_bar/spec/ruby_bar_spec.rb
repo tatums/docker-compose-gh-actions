0f5e1f2f54f16c246193690937c9aa9e66222740
Tatum Szymczak <tatum@ashlandstudios.com>
2020-11-15 16:20:25 -0600

    parallel

--- ruby_bar/spec/ruby_bar_spec.rb
+++ ruby_bar/spec/ruby_bar_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe RubyBar do
   end
 
   it "can redis" do
+    puts `echo HOST: $HOSTNAME`
+    puts "RUNNER: #{ENV["RUNNER"]}"
     RubyBar.set("a", "b")
     value = RubyBar.get("a")
     expect(value).to eq("b")
